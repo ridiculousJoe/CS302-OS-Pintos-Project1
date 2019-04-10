4eaca0630444f2be58cb2557d593c259ef342fd1
ridiculousJoe <11611026@mail.sustech.edu.cn>
2019-04-10 13:53:16 +0800

    finish pro 1 final report first part, add a disable interrupt for get priority

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -368,7 +368,10 @@ thread_set_priority (int new_priority)
 int
 thread_get_priority (void) 
 {
-  return thread_current ()->priority;
+  enum intr_level old_level = intr_disable ();
+  int val =  thread_current ()->priority;
+  intr_set_level (old_level);
+  return val;
 }
 
 /* Sets the current thread's nice value to NICE. */
